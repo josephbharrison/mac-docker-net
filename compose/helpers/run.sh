012dd3ed96c0e56dfea20c0443896c4253e2f99c
Joe Harrison <joseph.harrison@windstream.com>
2023-03-03 22:22:01 -0500

    pipeline test

---
+++ compose/helpers/run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+dest=${dest:-docker.ovpn}
+
+if [ ! -f "/local/$dest" ]; then
+    echo "*** REGENERATING ALL CONFIGS ***"
+    set -ex
+    # rm -rf /etc/openvpn/*
+    ovpn_genconfig -u tcp://localhost
+    sed -i 's|^push|#push|' /etc/openvpn/openvpn.conf
+    echo localhost | ovpn_initpki nopass
+    easyrsa build-client-full host nopass
+    ovpn_getclient host | sed '
+    	s|localhost 1194|localhost 13194|;
+	s|redirect-gateway.*|route 172.16.32.0 255.255.240.0|;
+    ' > "/local/$dest"
+fi
+
+# Workaround for https://github.com/wojas/docker-mac-network/issues/6
+/sbin/iptables -I FORWARD 1 -i tun+ -j ACCEPT
+
+#### /sbin/iptables -I FORWARD 1 -i tun+ -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+#### /sbin/iptables -A INPUT -i tun+ -j ACCEPT
+#### /sbin/iptables -A FORWARD -i tun+ -j ACCEPT
+#### /sbin/iptables -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#### /sbin/iptables -A FORWARD -i eth0 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+exec ovpn_run
---
+++ docker-net.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+opt=$1
+
+[[ -f ~/.provile ]] && source ~/.profile
+[[ -f venv/bin/activate ]] && source venv/bin/activate
+
+#project=${PWD##*/}
+proj_home=$CODE_HOME/mac-docker-net
+
+if [[ -d $proj_home && -z $opt ]];then
+  cd $proj_home
+  opt="-s"
+fi
+
+if [[ -z "$opt" || $opt == "-h" || $opt == "--help" ]];then
+  echo "
+Usage: docker-net.sh [OPTIONS]
+
+Options:
+    -b    Build
+    -s    Start
+    -x    Stop
+    -l    Update /etc/hosts entries and lo0 alias
+    -H    Update /etc/hosts entries only
+    -h    Help message
+"
+  exit 0
+fi
+
+
+[[ $(uname -a | grep -c "arm64") -gt 0 ]] &&  export DOCKER_DEFAULT_PLATFORM=linux/amd64 
+
+if [[ $opt == "-H" || $opt == "-l" ]];then
+
+  ansible --version > /dev/null || brew install ansible
+  echo '[defaults]' > ansible.cfg
+  echo "interpreter_python=$(which python)" >> ansible.cfg
+
+  echo '[containers]' > inventory
+  for container_id in $(docker ps --format '{{.ID}}')
+  do
+    container_ip=$(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${container_id})
+    container_name=$(docker inspect --format '{{.Name}}' ${container_id} | tr -d '/')
+    container_network=`docker network inspect --format='{{.Name}}' $(docker inspect --format='{{range .NetworkSettings.Networks}}{{.NetworkID}}{{end}}'  ${container_id})`
+    [[ -z $container_ip ]] && container_ip=127.0.0.1
+    echo "${container_name} host=${container_name} ip=${container_ip}" >> inventory
+    if [[ $opt == "-l" ]];then
+      sudo ifconfig lo0 alias ${container_ip}/20
+    fi
+  done
+
+  ## uncomment line below if '-b' does not execute as root 
+  #ansible-playbook -i inventory playbook.yml --sudo
+  ansible-playbook -i inventory playbook.yml -b
+
+fi
+
+export PROJECT=mac-docker-net
+
+if [[ $opt == "-b" ]];then
+  (
+    cd openvpn
+    docker buildx build . -t openvpn
+  )
+  cd compose 
+  test=$(docker network inspect $PROJECT &> /dev/null)
+  [[ $? -gt 0 ]] && docker network create --subnet "172.16.33.0/24" --gateway "172.16.33.1" $PROJECT
+  docker compose -f docker-network.yml -f docker-compose.yml -p $PROJECT build
+fi
+
+if [[ $opt == "-s" ]];then
+  cd compose
+  docker compose -f docker-network.yml -f docker-compose.yml -p $PROJECT up -d
+fi
+
+if [[ $opt == "-x" ]];then
+  cd compose
+  docker compose -f docker-network.yml -f docker-compose.yml -p $PROJECT stop
+fi
+
+exit 0

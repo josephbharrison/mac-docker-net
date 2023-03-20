c7c5169e1b051ef3b05a45d8f56a7be262afccfc
Joe Harrison <joseph.harrison@windstream.com>
2023-03-20 16:32:34 -0400

    pipeline test

--- compose/helpers/run.sh
+++ compose/helpers/run.sh
@@ -9,7 +9,7 @@ if [ ! -f "/local/$dest" ]; then
     ovpn_genconfig -u tcp://localhost
     sed -i 's|^push|#push|' /etc/openvpn/openvpn.conf
     echo localhost | ovpn_initpki nopass
-    easyrsa build-client-full host nopass
+    easyrsa build-client-full host nopass -y
     ovpn_getclient host | sed '
     	s|localhost 1194|localhost 13194|;
 	s|redirect-gateway.*|route 172.16.32.0 255.255.240.0|;
